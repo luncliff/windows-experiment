0156e8559aaa0420f58ab91c6a648edb7c1914d6
luncliff <luncliff@gmail.com>
2023-07-23 20:27:42 +0900

    build: use vcpkg manifest to install dependencies
    * use spdlog and create format helper
    * vcxproj: set VcpkgEnableManifest property

--- App1/App.xaml.cpp
+++ App1/App.xaml.cpp
@@ -3,29 +3,37 @@
 #include "App.xaml.h"
 #include "MainWindow.xaml.h"
 
+#include "winrt_fmt_helper.hpp"
+#include <spdlog/spdlog.h>
+
 namespace winrt::App1::implementation {
 
+using namespace std::placeholders;
 using namespace Windows::Foundation;
-using namespace Microsoft::UI::Xaml;
 using namespace Microsoft::UI::Xaml::Controls;
 using namespace Microsoft::UI::Xaml::Navigation;
 
 App::App() {
     InitializeComponent();
+    set_log_stream("App");
 
 #if defined _DEBUG && !defined DISABLE_XAML_GENERATED_BREAK_ON_UNHANDLED_EXCEPTION
-    UnhandledException([this](IInspectable const&, UnhandledExceptionEventArgs const& e) {
-        if (IsDebuggerPresent() == false)
-            // ...
-            return;
-
-        auto message = e.Message();
-        __debugbreak();
-    });
+    //auto handler = std::bind(&App::OnUnhandledException, this, _1, _2);
+    UnhandledException({this, &App::OnUnhandledException});
 #endif
 }
 
+void App::OnUnhandledException(IInspectable const&, UnhandledExceptionEventArgs const& e) {
+    if (IsDebuggerPresent() == false)
+        // ...
+        return;
+    winrt::hstring txt = e.Message();
+    spdlog::critical("{}: {}", "App", txt);
+    __debugbreak();
+}
+
 void App::OnLaunched(LaunchActivatedEventArgs const&) {
+    spdlog::info("{}: {}", "App", __func__);
     window = make<MainWindow>();
     window.Activate();
 }
