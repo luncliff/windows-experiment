1bfcc2bbeeeb9e54793af3b6abb54bf86a86d6ae
luncliff <luncliff@gmail.com>
2023-11-21 01:42:03 +0900

    ui: basic ListView with MIDL types

--- App1/TestPage1.xaml.cpp
+++ App1/TestPage1.xaml.cpp
@@ -10,6 +10,8 @@ namespace winrt::App1::implementation {
 using namespace Microsoft::UI::Xaml;
 
 TestPage1::TestPage1() {
+    InitializeComponent();
+    viewmodel0 = winrt::make<App1::implementation::RepositoryViewModel>();
     //...
 }
 
@@ -22,7 +24,26 @@ void TestPage1::Page_Unloaded(IInspectable const&, RoutedEventArgs const&) {
     spdlog::info("{}: {}", "TestPage1", __func__);
 }
 
-void TestPage1::myButton_Click(IInspectable const&, RoutedEventArgs const&) {
-    myButton().Content(box_value(L"Clicked"));
+App1::RepositoryViewModel TestPage1::ViewModel0() {
+    return viewmodel0;
+}
+
+fire_and_forget TestPage1::on_button_clicked(IInspectable const& s, RoutedEventArgs const&) {
+    if (auto button = s.try_as<Button>(); button != OpenButton())
+        spdlog::debug("{}: {}", "TestPage1", "sender is OpenButton");
+    if (selected == nullptr)
+        co_return;
+    auto launched = co_await Windows::System::Launcher::LaunchUriAsync(selected.ProjectUri());
+    if (launched == false)
+        spdlog::error("{}: {}", "TestPage1", "launch failed");
+}
+
+void TestPage1::on_selection_changed(IInspectable const& s, SelectionChangedEventArgs const&) {
+    auto view = s.try_as<Microsoft::UI::Xaml::Controls::ListView>();
+    if (view == nullptr)
+        return;
+    auto item = view.SelectedItem();
+    selected = item.try_as<RepositoryItem>();
+    spdlog::info("{}: selected {}", "TestPage1", winrt::to_string(selected.Name()));
 }
 } // namespace winrt::App1::implementation
--- App1/TestPage1.xaml.h
+++ App1/TestPage1.xaml.h
@@ -2,22 +2,32 @@
 
 #include "TestPage1.g.h"
 
+#include "RepositoryViewModel.h"
+
 namespace winrt::App1::implementation {
 using Microsoft::UI::Xaml::RoutedEventArgs;
-using Microsoft::UI::Xaml::SizeChangedEventArgs;
-using Microsoft::UI::Xaml::Controls::SwapChainPanel;
+using Microsoft::UI::Xaml::Controls::Button;
+using Microsoft::UI::Xaml::Controls::SelectionChangedEventArgs;
 using Microsoft::UI::Xaml::Input::PointerRoutedEventArgs;
 using Microsoft::UI::Xaml::Input::TappedRoutedEventArgs;
 using Windows::Foundation::IAsyncAction;
 using Windows::Foundation::IInspectable;
 
 struct TestPage1 : TestPage1T<TestPage1> {
+  private:
+    App1::RepositoryViewModel viewmodel0 = nullptr;
+    App1::RepositoryItem selected = nullptr;
+
+  public:
     TestPage1();
 
     void Page_Loaded(IInspectable const&, RoutedEventArgs const&);
     void Page_Unloaded(IInspectable const&, RoutedEventArgs const&);
 
-    void myButton_Click(IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    App1::RepositoryViewModel ViewModel0();
+
+    fire_and_forget on_button_clicked(IInspectable const&, RoutedEventArgs const&);
+    void on_selection_changed(IInspectable const&, SelectionChangedEventArgs const&);
 };
 } // namespace winrt::App1::implementation
 
