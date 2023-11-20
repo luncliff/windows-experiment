2e64e17a9902e19cd8e1d0ffa5104c6791eb8499
luncliff <luncliff@gmail.com>
2023-11-21 01:01:25 +0900

    ui: create settings page

---
+++ App1/SettingsPage.xaml.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "SettingsPage.g.h"
+
+namespace winrt::App1::implementation {
+using Microsoft::UI::Xaml::Navigation::NavigationEventArgs;
+
+struct SettingsPage : SettingsPageT<SettingsPage> {
+    SettingsPage();
+
+    void OnNavigatedTo(const NavigationEventArgs& e);
+    void OnNavigatedFrom(const NavigationEventArgs&);
+};
+} // namespace winrt::App1::implementation
+
+namespace winrt::App1::factory_implementation {
+struct SettingsPage : SettingsPageT<SettingsPage, implementation::SettingsPage> {};
+} // namespace winrt::App1::factory_implementation
