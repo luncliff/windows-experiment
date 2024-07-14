496e7e65677df8780971f3a4d5f73bbf8081f041
luncliff <luncliff@gmail.com>
2024-07-15 00:23:34 +0900

    vstest: add test for DeviceResources

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -1,11 +1,28 @@
 #include <CppUnitTest.h>
 
+// https://learn.microsoft.com/en-us/visualstudio/test/microsoft-visualstudio-testtools-cppunittestframework-api-reference?view=vs-2022
+#include <functional>
+
+#include "DeviceResources.h"
+
 using Microsoft::VisualStudio::CppUnitTestFramework::Assert;
 using Microsoft::VisualStudio::CppUnitTestFramework::TestClass;
 
-class UnitTest1 : public TestClass<UnitTest1> {
-    TEST_METHOD(TestMethod1) {
-        // ...
-        Assert::AreEqual(0, 0);
+class DeviceResourcesTest : public TestClass<DeviceResourcesTest> {
+    DX::DeviceResources res{};
+
+    TEST_METHOD_INITIALIZE(Initialize) {
+        res.CreateDeviceResources();
+        res.WaitForGpu();
+    }
+    TEST_METHOD_CLEANUP(Cleanup) {
+        res.WaitForGpu();
+    }
+
+    TEST_METHOD(TestInvalidHWND) {
+        HWND windows = nullptr;
+        res.SetWindow(windows, 0, 0);
+        Assert::ExpectException<std::exception>( // invalid argument
+            std::bind_front(&DX::DeviceResources::CreateWindowSizeDependentResources, &res));
     }
 };
