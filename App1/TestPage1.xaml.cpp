01553245b7cc4357b014a5767ca81db20a208ed1
luncliff <luncliff@gmail.com>
2025-07-11 00:18:12 +0900

    ui: use DeviceResources functions in Page events

--- App1/TestPage1.xaml.cpp
+++ App1/TestPage1.xaml.cpp
@@ -10,35 +10,47 @@
 
 #include "BasicViewModel.h"
 #include "StepTimer.h"
-#include <microsoft.ui.xaml.media.dxinterop.h> // ISwapChainPanelNative for Microsoft namespace
 
 namespace winrt::App1::implementation {
 
-TestPage1::TestPage1() {
+TestPage1::TestPage1() noexcept(false) {
     // Initialize page, resources ...
     resources.CreateDeviceResources();
-    DX::StepTimer timer{};
 }
 
 App1::BasicViewModel TestPage1::ViewModel() noexcept {
     return viewmodel0;
 }
 
-void TestPage1::SwapChainPanel0_SizeChanged(IInspectable const& sender, SizeChangedEventArgs const& e) {
+void TestPage1::OnDeviceLost() noexcept {
+    // render targets, swapchains will be removed. disconnect
+    bridge->SetSwapChain(nullptr);
+}
+
+void TestPage1::OnDeviceRestored() noexcept {
+    // ... resources.CreateDeviceResources() is already done ...
+    // CreateWindowSizeDependentResources ...
+    if (IDXGISwapChain* swapchain = resources.GetSwapChain(); swapchain != nullptr)
+        bridge->SetSwapChain(swapchain);
+}
+
+void TestPage1::panel0_size_changed(IInspectable const& sender, SizeChangedEventArgs const& e) {
     // get the new size and update the resources
     auto size = e.NewSize();
     resources.CreateWindowSizeDependentResources(static_cast<UINT>(size.Width), static_cast<UINT>(size.Height));
     // note the sender is SwapChainPanel
-    winrt::com_ptr<ISwapChainPanelNative> m_bridge = nullptr;
-    sender.as(m_bridge);
+    sender.as(bridge);
     if (IDXGISwapChain* swapchain = resources.GetSwapChain(); swapchain != nullptr)
-        m_bridge->SetSwapChain(swapchain);
+        bridge->SetSwapChain(swapchain);
 }
 
 void TestPage1::OnNavigatedTo(const NavigationEventArgs& e) {
+    // we are now visible. connect for the future notifications
+    resources.RegisterDeviceNotify(this);
+
+    // no parameter means no ViewModel is forwarded
     if (e == nullptr)
         return;
-
     // Get the ViewModel from the navigation parameter
     if (e.Parameter() != nullptr) {
         viewmodel0 = e.Parameter().try_as<App1::BasicViewModel>();
@@ -52,18 +64,26 @@ void TestPage1::OnNavigatedTo(const NavigationEventArgs& e) {
 }
 
 void TestPage1::OnNavigatedFrom(const NavigationEventArgs&) {
-    // Handle leaving the page via navigation
+    // the page will be destroyed soon. remove the connection
+    OnDeviceLost();
+    resources.RegisterDeviceNotify(nullptr);
 }
 
-void TestPage1::OnTestButtonClick(winrt::Windows::Foundation::IInspectable const&, RoutedEventArgs const&) {
+void TestPage1::on_test_button_click(IInspectable const&, RoutedEventArgs const&) {
     // Update the status text when button is clicked
-    if (viewmodel0) {
-        auto itemCount = viewmodel0.Items().Size();
-        StatusTextBlock().Text(winrt::hstring(L"Button clicked! TestPage1 is working correctly with ") +
-                               winrt::to_hstring(itemCount) + L" items in the ViewModel.");
+    App1::BasicViewModel viewmodel = ViewModel();
+    if (viewmodel) {
+        auto itemCount = viewmodel.Items().Size();
+        auto message = std::format(L"Button clicked! TestPage1 has {} items in the ViewModel.", itemCount);
+        StatusTextBlock().Text(message);
     } else {
         StatusTextBlock().Text(L"Button clicked! TestPage1 is working correctly, but no ViewModel available.");
     }
+    // todo: add PIX capture
+    // todo: add basic rendering logic
+    resources.Prepare();
+    // ...
+    resources.Present();
 }
 
 } // namespace winrt::App1::implementation
--- App1/TestPage1.xaml.h
+++ App1/TestPage1.xaml.h
@@ -4,27 +4,33 @@
 #include "DeviceResources.h"
 #include "TestPage1.g.h"
 
+#include <microsoft.ui.xaml.media.dxinterop.h> // ISwapChainPanelNative for Microsoft namespace
+
 namespace winrt::App1::implementation {
 using Microsoft::UI::Xaml::RoutedEventArgs;
 using Microsoft::UI::Xaml::Navigation::NavigationEventArgs;
 using winrt::Microsoft::UI::Xaml::SizeChangedEventArgs;
 using winrt::Windows::Foundation::IInspectable;
 
-struct TestPage1 : TestPage1T<TestPage1> {
+struct TestPage1 : TestPage1T<TestPage1>, DX::IDeviceNotify {
   private:
     DX::DeviceResources resources{DXGI_FORMAT_B8G8R8A8_UNORM, DXGI_FORMAT_D24_UNORM_S8_UINT, 2};
+    winrt::com_ptr<ISwapChainPanelNative> bridge = nullptr;
     App1::BasicViewModel viewmodel0{nullptr};
 
   public:
-    TestPage1();
+    TestPage1() noexcept(false);
 
     void OnNavigatedTo(const NavigationEventArgs&);
     void OnNavigatedFrom(const NavigationEventArgs&);
 
-    void OnTestButtonClick(IInspectable const&, RoutedEventArgs const&);
-    void SwapChainPanel0_SizeChanged(IInspectable const&, SizeChangedEventArgs const&);
+    void on_test_button_click(IInspectable const&, RoutedEventArgs const&);
+    void panel0_size_changed(IInspectable const&, SizeChangedEventArgs const&);
 
     App1::BasicViewModel ViewModel() noexcept;
+
+    void OnDeviceLost() noexcept override;
+    void OnDeviceRestored() noexcept override;
 };
 } // namespace winrt::App1::implementation
 
